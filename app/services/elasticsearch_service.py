e503fa8d07655548aade8c32682b72d6593ce286
Saurabh Saxena <sau.5rabh88@gmail.com>
2020-04-23 23:02:02 +0530

    Add Client routes

---
+++ app/services/elasticsearch_service.py
@@ -0,0 +1,16 @@
+from elasticsearch.client import Elasticsearch as es
+import logging
+from app.config.config import ConfigType
+
+logger = logging.getLogger('gunicorn.error')
+
+
+class ElasticSearchService(object):
+    def __init__(self, es_client=None, config_object: ConfigType = None):
+        self._es_connection: es = None
+        self._es_client = es_client
+        self._config_object = config_object
+
+    def init_service(self, es_client, config_object: ConfigType):
+        self._es_connection = es_client.redis_connection
+        self._config_object = config_object
